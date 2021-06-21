b6e7105b057bbc3e0c98c3e1358143a8eb6c329a
Jay Karp <jaykarp2@gmail.com>
2021-06-21 00:28:52 -0400

    Updates

---
+++ precache-manifest.62b548758f5ab0ab105eefb3e87f4574.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495e1802be0a098064ec65afeb9266d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8310e68ce76e15132439",
+    "url": "/static/css/main.34ddbca1.chunk.css"
+  },
+  {
+    "revision": "0e865705ad975e26b322",
+    "url": "/static/js/2.d3b39e85.chunk.js"
+  },
+  {
+    "revision": "9c51a917553d6ebea4909433ec974940",
+    "url": "/static/js/2.d3b39e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8310e68ce76e15132439",
+    "url": "/static/js/main.ac78cb2e.chunk.js"
+  },
+  {
+    "revision": "131f9a8cf7dd01b5b5f3",
+    "url": "/static/js/runtime-main.fb1aba0e.js"
+  },
+  {
+    "revision": "1f8f38c5d9be690dd979d6d645a90ebf",
+    "url": "/static/media/ShopImage.1f8f38c5.png"
+  },
+  {
+    "revision": "20fd8e32d2273c98b760958eed1629a8",
+    "url": "/static/media/about.20fd8e32.jpeg"
+  },
+  {
+    "revision": "39d15d870600169336d04fd6b85195f5",
+    "url": "/static/media/contact.39d15d87.jpg"
+  }
+]);
